59ef997d2e41859b6de5ab4cf04361dc44260ecd
Arnab Deka <arnab.deka@gmail.com>
2014-01-26 18:29:18 +0530

    A jetty Hello world web service.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clojudo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Clojure Todo-list service to test with"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-jetty-adapter "1.1.6"]])
--- test/clojudo/core_test.clj
+++ test/clojudo/core_test.clj
@@ -1,7 +1,3 @@
 (ns clojudo.core-test
   (:require [clojure.test :refer :all]
             [clojudo.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
